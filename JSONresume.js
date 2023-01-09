82c583b3a06b2daf18ee7f28e9cc13e27eccd9eb
Sankar4595 <120165724+Sankar4595@users.noreply.github.com>
2023-01-09 20:28:28 +0530

    Add files via upload

---
+++ JSONresume.js
@@ -0,0 +1,17 @@
+var resume = [{
+    "name" : "Sankaranarayanan",
+    "dateOfBirth" : "04/05/1995",
+    "age" : "28",
+    "address" : "Ashok nagar,dharapuram",
+    "phoneNumber" : "9976875417",
+    "mailId" : "95sankar@gmail.com",
+    "educationQualification" : "B.E Mechanical",
+    "score" : "6.9grade",
+    "softwareSkills" :"JavaScript,HTML,CSS",
+    "workingExperience" : "5yearsMechanicalExperience",
+    "extraCuricularActivities" : "cricket,chess"    
+}];
+
+for(let i in resume){
+    console.log(resume[i]);
+}
