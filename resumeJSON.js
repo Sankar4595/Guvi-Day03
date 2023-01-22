619bc3de049b81ac0b7d8aff47a7345344d9348c
Sankar4595 <120165724+Sankar4595@users.noreply.github.com>
2023-01-22 08:28:52 +0530

    Add files via upload

---
+++ resumeJSON.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "SANKARANARAYANAN S",
+      "email": "95sankaar@gamil.com",
+      "phone": 9976875417,
+      "degree": "B.E",
+      "location": {
+        "address": "109/15b Ashok nagar,",
+        "postalCode": 638656,
+        "city": "Dharapuram",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/sankaranarayanan-saminathan-675359a0/",
+          "github":"https://github.com/Sankar4595"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Aquasub Engineering",
+        "position": "Production incharge",
+        "startDate": "2014-06-02",
+        "endDate": "2018-11-21",
+        "summary": "i am the production incharge handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Karpagam university, coimbatore",
+        "department": "Mechanical",
+        "studyType": "Part time",
+        "batch start year": 2015,
+        "batch end year": 2018,
+        "gpa": 6.9,
+      }
+    ],
+    "skills": [
+      {
+        "name": "HTML,javascript",
+        "level": "beginer",
+        "project": [
+          "random number show in html site using javscript"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Football playing",
+      }
+    ]
+  }
+  console.log(myResume);
